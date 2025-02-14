b33182c434dea846f64f69cec5e8b9d243144dea
jgh진건희 <s23054@gsm.hs.kr>
2025-02-14 13:17:15 +0900

    하단바 버그 수리

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,18 +9,18 @@ import { CgProfile } from "react-icons/cg";
 
 function Footer() {
     const router = useRouter();
-    const pathname = typeof window !== "undefined" ? window.location.pathname : ""; // 현재 경로 확인
+    const pathname = typeof window !== "undefined" ? window.location.pathname || "/" : "/"; // 초기값을 "/"로 설정
 
     const getIconColor = (path: string) => {
-        return pathname === path ? "#FF412E" : "#d9d9d9"; // 현재 경로면 #FF412E, 아니면 기본색 #d9d9d9
+        return pathname === path ? "#FF412E" : "#d9d9d9";
     };
 
     return (
         <S.FooterCover>
-            <BsFillPeopleFill size={25} color={getIconColor("/geshipan")} onClick={() => router.push("")} onTouchStart={(e) => e.stopPropagation()} />
+            <BsFillPeopleFill size={25} color={getIconColor("/geshipan")} onClick={() => router.push("/geshipan")} onTouchStart={(e) => e.stopPropagation()} />
             <CiChat2 size={25} color={getIconColor("/Q")} onClick={() => router.push("/Q")} onTouchStart={(e) => e.stopPropagation()} />
             <FaHome size={25} color={getIconColor("/")} onClick={() => router.push("/")} onTouchStart={(e) => e.stopPropagation()} />
-            <FaBookOpen size={25} color={getIconColor("/Archaive")} onClick={() => router.push("/")} onTouchStart={(e) => e.stopPropagation()} />
+            <FaBookOpen size={25} color={getIconColor("/Archaive")} onClick={() => router.push("/Archaive")} onTouchStart={(e) => e.stopPropagation()} />
             <CgProfile size={25} color={getIconColor("/Profile")} onClick={() => router.push("/Profile")} onTouchStart={(e) => e.stopPropagation()} />
         </S.FooterCover>
     );
