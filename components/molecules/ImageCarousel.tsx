1fbcdb95c760ed2ea974406e2e7eee64e60e930b
mjdefined <pmj.defined@gmail.com>
2025-09-13 00:55:40 +0900

    feat: ImageCarousel 컴포넌트 추가

---
+++ components/molecules/ImageCarousel.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+
+export const ImageCarousel: FC<{
+  images: string[];
+  showButtons?: boolean;
+  maxContainerHeight?: string;
+}> = ({ images, showButtons = false }) => {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent className="-ml-2 md:-ml-4">
+        {images.map((image, index) => (
+          <CarouselItem key={index} className="pl-2 md:pl-4">
+            <div className="flex aspect-video items-center justify-center rounded-lg bg-gray-50 p-1">
+              <img
+                src={image}
+                alt={`Image ${index + 1}`}
+                className="max-h-[25rem] w-full rounded-lg object-contain"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      {images.length > 1 && (
+        <div className="mt-3 flex items-center justify-center space-x-2">
+          <div className="text-sm text-gray-500">
+            {images.length}개의 이미지
+          </div>
+        </div>
+      )}
+      {showButtons && images.length > 1 && (
+        <>
+          <CarouselPrevious />
+          <CarouselNext />
+        </>
+      )}
+    </Carousel>
+  );
+};
