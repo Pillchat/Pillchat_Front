e229912ba0d0e4dfab6e42686845391cde888a82
mjdefined <pmj.defined@gmail.com>
2025-10-18 01:34:08 +0900

    feat: 각 과목의 정보를 불러오는  API 추가

---
+++ app/api/subjects/[code]/route.ts
@@ -0,0 +1,25 @@
+import { serverFetch } from "@/lib/functions";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ code: string }> },
+) {
+  try {
+    const { code } = await context.params;
+    const data = await serverFetch(`/api/subjects/${code}`, {
+      method: "GET",
+      request,
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("과목 상세 조회 API 에러:", error);
+    const errorMessage = error instanceof Error ? error.message : "{}";
+    const errorInfo = JSON.parse(errorMessage);
+    return NextResponse.json(
+      { message: errorInfo.message || "과목을 불러오는데 실패했습니다." },
+      { status: errorInfo.status || 500 },
+    );
+  }
+}
